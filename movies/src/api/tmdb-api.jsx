be29ff549a49af085c666f25918764fca2f29de9
Harry OShea <20107680@setu.ie>
2025-10-29 23:30:38 +0000

    Added reccomended similar movies and tabs to movie details

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -180,3 +180,22 @@ export const getTrendingMovies = ({ timeWindow = "week", language = "en-US" } = 
       throw error
    });
   };
+
+export const getMovieRecommendations = (movieId) => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${
+    import.meta.env.VITE_TMDB_KEY
+  }`;
+
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
--- movies/src/pages/movieDetailsPage.jsx
+++ movies/src/pages/movieDetailsPage.jsx
@@ -6,10 +6,15 @@ import { getMovie } from '../api/tmdb-api'
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner'
 // import useMovie from "../hooks/useMovie";   Redundant
+import MovieRecommendations from "../components/movieRecommendations";
+import { Tabs, Tab, Box } from "@mui/material";
+import { useState } from "react";
 
 const MoviePage = (props) => {
   const { id } = useParams();
-    const { data: movie, error, isPending, isError  } = useQuery({
+  const [tab, setTab] = useState(0);
+
+  const { data: movie, error, isPending, isError  } = useQuery({
     queryKey: ['movie', {id: id}],
     queryFn: getMovie,
   })
@@ -28,7 +33,16 @@ const MoviePage = (props) => {
       {movie ? (
         <>
           <PageTemplate movie={movie}>
-            <MovieDetails movie={movie} />
+            
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs value={tab} onChange={(e, v) => setTab(v)}>
+            <Tab label="Overview" />
+            <Tab label="Recommendations" />
+            </Tabs>
+            </Box>
+
+            {tab === 0 && <MovieDetails movie={movie} />}
+            {tab === 1 && <MovieRecommendations movieId={id} />}
           </PageTemplate>
         </>
       ) : (
